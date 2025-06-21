ad0e076a19b253f82a1dcf092601820bf49c29d1
Luis Zancanela <luisczdidi@gmail.com>
2025-06-21 11:53:50 -0300

    init: first commit

---
+++ src/test/java/br/dev/zancanela/taskboard/TaskboardApplicationTests.java
@@ -0,0 +1,16 @@
+package br.dev.zancanela.taskboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+		// This test checks if the Spring application context loads successfully.
+		// If there are any issues with the configuration or beans, this test will fail.
+		// No additional assertions are needed here as the context loading itself is the test.
+	}
+
+}
